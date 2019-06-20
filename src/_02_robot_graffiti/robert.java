de4f4daee0d31710fb801d536e3bf40baee079d5
ilabstaff <ilabstaff@172.16.116.69>
2019-06-19 17:46:46 -0700

    graffiti

---
+++ src/_02_robot_graffiti/robert.java
@@ -0,0 +1,24 @@
+package _02_robot_graffiti;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class robert {
+public static void main(String[] args) {
+	Robot rob=new Robot();
+rob.penDown();
+rob.setRandomPenColor();
+rob.move(100);
+rob.turn(80);
+rob.move(100);
+rob.turn(80);
+rob.move(90);
+rob.turn(70);
+rob.move(80);
+rob.turn(60);
+rob.move(200);
+
+
+
+
+}
+}
